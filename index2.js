c98efff5dac0150ae7b1995f4b0a77189f1cd435
manishman <manishnm@hotmail.com>
2020-06-12 20:10:52 -0400

    rotate

--- index2.js
+++ index2.js
@@ -1,12 +1,7 @@
 
-alert("Welcome Veterans!!");
+function makeSmall() {
+    var i = 0;
+    document.getElementById("veteran").setAttribute('style', 'transform:rotate(180deg)') ;
+}
+  
 
-
-let vet = document.querySelector('#demo')
-vet.innerHTML = "Veteran Resources"
-vet.addEventListner{'mouseover', fucntion(){
-    vet.style.color = 'blue'
-    vet.style.textShadow = "0.075em 0.08em 0.1em rgba(0, 0, 0, 1)"
-}}
-
-vet.addEventListener('mouseout')
