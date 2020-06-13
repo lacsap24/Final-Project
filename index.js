7cb17f3a59795d17a8895955180065095246b223
manishman <manishnm@hotmail.com>
2020-06-12 20:42:30 -0400

    jlkjl

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
-document.addEventListener('DOMContentLoaded', function(){
 
-    let changeColorButton = document.querySelector('#changeColor')
-  
-    let colorDiv = document.querySelector('#colorDiv')
-  
-    changeColorButton.addEventListener('click', function(){
-  
-        colorDiv.style.background = 'green'
-  
-    })
-  
-  })
 
-  function makeSmall() {
-      document.getElementById("veteran").setAttribute(
-          'style', 'transform:rotate(180deg)'
+function makeSmall() {
+  document.getElementById("veteran").setAttribute(
+      'style', 'transform:rotate(180deg)'
+  ) ;
+}
 
-      )
+function myFunction() {
+    document.getElementById("veteran").innerHTML = "Hello World";
   }
-
-
-
-  alert("Welcome Veterans!!");
